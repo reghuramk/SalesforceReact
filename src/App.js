658eb89078c0af82f41fe90078a70445a9a703e6
Reghuram Karunamurthi <54064162+reghuramk@users.noreply.github.com>
2021-04-01 23:26:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import GlobalHeader from './components/GlobalHeader'
+import GlobalNavBar from './components/GlobalNavigationBar'
+
+
+
+
+const App = () => {
+  return  (
+    <div>
+
+    <GlobalHeader/>  
+    
+
+    </div>
+  )
+}
+
+export default App
+
